31ed7f16bd91b89912a9689f2fc8de5cbb11bcf3
SIVAKUMARDEPURU <110371695+SIVAKUMARDEPURU@users.noreply.github.com>
2022-08-05 11:50:11 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,10 +12,35 @@ resource "aws_instance" "instance1" {
   root_block_device {
     volume_size = var.volume_size
   }
-  tags={
+   tags={
     Name= "instance-${count.index}"
   }
 }
+resource "aws_cloudwatch_metric_alarm" "disk_percentage_low" {
+  for_each                  = toset(var.instance)
+  alarm_name                = "disk_percentage_low"  
+  comparison_operator       = "LessThanOrEqualToThreshold"  
+  evaluation_periods        = "1"                  
+  metric_name               = "LogicalDisk % Free Space"
+  namespace                 = "AWS/EC2"  
+  period                    = "60"   
+  statistic                 = "Average"            
+  threshold                 = "20"
+  alarm_description         = "This metric monitors ec2 disk utilization"   
+  actions_enabled           = "true"    
+  #alarm_actions             = [aws_sns_topic.disk_alarm.arn]   
+  insufficient_data_actions = []
+  
+  dimensions = {
+    InstanceId   = "i-0c4b0314e50652017"
+    InstanceType = "t2.micro"
+    instance     = each.value
+             } 
+}
+
+
+
+
 resource "aws_security_group" "instance" {
   description = var.sec_group_description
   egress = [
